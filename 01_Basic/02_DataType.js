2602c5fa8acf6ed0b8a4bf5a06f0d7109412b3b3
ankitsingh015 <ankitkumarbkj39@gmail.com>
2024-07-21 11:50:04 +0530

    Add Basic of JavaScript

---
+++ 01_Basic/02_DataType.js
@@ -0,0 +1,37 @@
+"use strict";   //treat all JS code as newer version
+
+// alert(3+3)       we are using nodejs, not browser
+
+//Code should be Readable
+console.log(3 + 3);
+
+console.log("Hitesh");
+
+//https://tc39.es/ecma262/
+
+
+let name="Ankit"
+let age=18
+let isLoggedIn=false
+let state =""
+let count= null
+
+
+// number =>2 to power 53
+// bigint
+// string => ""
+// boolean => true/flase
+// null => standalone value
+// undefined
+// symbol   => unquie
+
+
+
+// object 
+
+
+console.log(typeof "hitesh")
+console.log(typeof age)
+console.log(typeof null)        // object
+console.log(typeof undefined)  //undefined
+
