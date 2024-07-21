c510e52df86b6c058aee7749f11d405bb979983a
ankitsingh015 <ankitkumarbkj39@gmail.com>
2024-07-21 11:46:12 +0530

    Add basic of Variable

---
+++ 01_Basic/01_Variable.js
@@ -0,0 +1,17 @@
+const accountId = 144553        //Value can't be change
+let accountEmail="ankitkumarbkj39@gmail.com"
+var accountPassword="ankit"     //Prefer Not To Use
+accountCity= "Jaipur"
+let accountState  
+
+// accountId =2  Not Allowed
+
+accountEmail="22bcs015@smvdu.ac.in"
+accountPassword="singh"
+accountCity="Bihar"
+
+console.log(accountId);
+
+//Prefer not to use var because of issue in block scope and finctional Scope
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
